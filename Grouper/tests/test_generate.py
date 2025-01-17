b895d6172ce000514d272aefd028d93e48c650ab
kierannp <nehilkieran@gmail.com>
2025-01-17 14:27:45 -0600

    Generalized nauty path

--- Grouper/tests/test_generate.py
+++ Grouper/tests/test_generate.py
@@ -1,29 +1,46 @@
-import time
-import pytest
-from Grouper import GroupGraph, exhaustive_generate, Node
-from Grouper.tests.base_test import BaseTest
 import logging
 
-class TestGeneration(BaseTest):
+import pytest
 
+from Grouper import Node, exhaustive_generate
+from Grouper.tests.base_test import BaseTest
+
+
+class TestGeneration(BaseTest):
+    @pytest.mark.skip(reason="Too slow for general testing")
     @pytest.mark.parametrize("n_nodes", [2, 3, 4, 5, 6])
     @pytest.mark.parametrize("num_procs", [1, 2, 4, 8, 16])
-    @pytest.mark.parametrize("node_defs", [[
-            # {"type": "t1", "smiles": "C", "ports": [0,1,2,3], "hubs": [0,0,0,0]},
-            {"type": "t2", "smiles": "N", "hubs": [0,0,0]},
-            {"type": "Methyl", "smiles": "C", "hubs": [0,0,0]},
-            # {"type": "Benzene", "smiles": "C1=CC=CC=C1", "ports": [0,1,2,3,4,5], "hubs": [0,1,2,3,4,5]},
-            {"type": "ester", "smiles": "C(=O)O", "hubs": [0,2]},
-            {"type": "extra1", "smiles": "O", "hubs": [0,0]}
-        ]]
+    @pytest.mark.parametrize(
+        "node_defs",
+        [
+            [
+                # {"type": "t1", "smiles": "C", "ports": [0,1,2,3], "hubs": [0,0,0,0]},
+                {"type": "t2", "smiles": "N", "hubs": [0, 0, 0]},
+                {"type": "Methyl", "smiles": "C", "hubs": [0, 0, 0]},
+                # {"type": "Benzene", "smiles": "C1=CC=CC=C1", "ports": [0,1,2,3,4,5], "hubs": [0,1,2,3,4,5]},
+                {"type": "ester", "smiles": "C(=O)O", "hubs": [0, 2]},
+                {"type": "extra1", "smiles": "O", "hubs": [0, 0]},
+            ]
+        ],
     )
-    def test_exhaustive_generate_performance(self, benchmark, n_nodes, num_procs, node_defs):
-        # Convert node_defs to the expected format
-        node_defs = set(Node(0, n["type"], n["smiles"], n["hubs"]) for n in node_defs)
+    def test_exhaustive_generate_performance(
+        self, benchmark, n_nodes, num_procs, node_defs
+    ):
+        # Load nauty path from config file
+        import json
+        import pathlib
+        print(pathlib.Path(__file__).parent.parent.parent / "config.json")
+        with open(pathlib.Path(__file__).parent.parent.parent / "config.json", "r") as config_file:
 
-        write_to_db = False
+            config = json.load(config_file)
+        nauty_path = config.get("nauty_path")
+        if not nauty_path:
+            raise RuntimeError("Nauty path is not defined in the configuration file.")
+
+        # Convert node_defs to the expected format
+        node_defs = set(Node(n["type"], n["smiles"], n["hubs"]) for n in node_defs)
+
         verbose = False
-        nauty_path = "/Users/kieran/projects/Grouper/packages/nauty2_8_8"
         input_file_path = ""
         positive_constraints = {}
         negative_constraints = set()
@@ -31,16 +48,56 @@ class TestGeneration(BaseTest):
         logging.info("Starting benchmark")
 
         benchmark(
-            exhaustive_generate, 
-            n_nodes, 
-            node_defs, 
-            nauty_path, 
-            input_file_path, 
-            num_procs, 
-            positive_constraints, 
-            negative_constraints, 
-            '',
-            verbose
+            exhaustive_generate,
+            n_nodes,
+            node_defs,
+            nauty_path,
+            input_file_path,
+            num_procs,
+            positive_constraints,
+            negative_constraints,
+            "",
+            verbose,
         )
         logging.info("Benchmark complete")
+    
+    def test_simple_exhaustive_generation(self):
+        node_defs = [
+            {"type": "t2", "smiles": "N", "hubs": [0, 0, 0]},
+            {"type": "Methyl", "smiles": "C", "hubs": [0, 0, 0]},
+            {"type": "ester", "smiles": "C(=O)O", "hubs": [0, 2]},
+            {"type": "extra1", "smiles": "O", "hubs": [0, 0]},
+        ]
+        # Load nauty path from config file
+        import json
+        import pathlib
+        print(pathlib.Path(__file__).parent.parent.parent / "config.json")
+        with open(pathlib.Path(__file__).parent.parent.parent / "config.json", "r") as config_file:
+            config = json.load(config_file)
+        nauty_path = config.get("nauty_path")
+        if not nauty_path:
+            raise RuntimeError("Nauty path is not defined in the configuration file.")
+        
+        # Convert node_defs to the expected format
+        node_defs = set(Node(n["type"], n["smiles"], n["hubs"]) for n in node_defs)
+
+        verbose = False
+        input_file_path = ""
+        positive_constraints = {}
+        negative_constraints = set()
+
+        logging.info("Starting simple generation")
+
+        exhaustive_generate(
+            2,
+            node_defs,
+            nauty_path,
+            input_file_path,
+            -1, # num_procs
+            positive_constraints,
+            negative_constraints,
+            "",
+            verbose,
+        )
+        logging.info("Simple generation complete")
 
