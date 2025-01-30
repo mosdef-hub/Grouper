9f996ad3760ad73e340c4d046b923ea4f92b3866
kierannp <nehilkieran@gmail.com>
2025-01-30 00:48:01 -0600

    Corrected test cases for new fragmentation output format

--- Grouper/tests/test_fragmentation.py
+++ Grouper/tests/test_fragmentation.py
@@ -20,7 +20,7 @@ class TestGroupGraph(BaseTest):
 
         out = fragment(smiles, node_defs)
 
-        assert out == truth
+        assert truth in out
 
     def test_fragment_2(self):
         node_defs = set()
@@ -36,7 +36,7 @@ class TestGroupGraph(BaseTest):
 
         out = fragment(smiles, node_defs)
 
-        assert out == truth
+        assert truth in out
 
     def test_empty_smiles(self):
         node_defs = set()
@@ -44,7 +44,7 @@ class TestGroupGraph(BaseTest):
 
         out = fragment(smiles, node_defs)
 
-        assert out == GroupGraph()
+        assert out == [GroupGraph()]
 
     def test_invalid_smiles(self):
         node_defs = set()
@@ -74,7 +74,7 @@ class TestGroupGraph(BaseTest):
 
         out = fragment(smiles, node_defs)
 
-        assert out == truth
+        assert truth in out
 
     def test_triple_node(self):
         node_defs = set()
@@ -93,7 +93,7 @@ class TestGroupGraph(BaseTest):
 
         out = fragment("O=COOC(=O)O", node_defs)
 
-        assert out == truth
+        assert truth in out
 
         truth = GroupGraph()
         truth.add_node("ester", "C(=O)O", [0, 2])
@@ -104,7 +104,7 @@ class TestGroupGraph(BaseTest):
 
         out = fragment("CNCNOC=O", node_defs)
 
-        assert out == truth
+        assert truth in out
 
         truth = GroupGraph()
         truth.add_node("alkene", "C", [0, 0, 0])
@@ -115,7 +115,7 @@ class TestGroupGraph(BaseTest):
 
         out = fragment("CNO", node_defs)
 
-        assert out == truth
+        assert truth in out
 
         truth = GroupGraph()
         truth.add_node("oxyl", "O", [0, 0])
@@ -126,7 +126,7 @@ class TestGroupGraph(BaseTest):
 
         out = fragment("O=C(O)OO", node_defs)
 
-        assert out == truth
+        assert truth in out
 
     def test_nodes_made_of_other_nodes(self):
         node_defs = set()
@@ -155,7 +155,7 @@ class TestGroupGraph(BaseTest):
 
         out = fragment("CCC(C)NCCNC", node_defs)
 
-        assert out == truth
+        assert truth in out
 
 
     def test_other_edge_cases(self):
@@ -172,7 +172,7 @@ class TestGroupGraph(BaseTest):
         node_defs = {node2, node1} # {node2}
         out = fragment("CCC", node_defs)
 
-        assert out == truth
+        assert truth in out
 
         truth = GroupGraph()
         truth.add_node("carbon", "C", [0,0,0,0])
@@ -182,28 +182,11 @@ class TestGroupGraph(BaseTest):
         truth.add_edge((2, 1), (1, 0))
 
         node1 = Group("4methyl", "C", [0,0,0,0])
-        node2 = Group("methanol", "CO", [0]) # methanol
-        node3 = Group("ether", "CO", [0,1]) # ether
+        node2 = Group("methanol", "CO", [0,0,0, 1]) # methanol
+        node3 = Group("ether", "CO", [0,0,0,1]) # ether
 
         node_defs = {node3, node2, node1} # {node2}
-        graph = fragment("CCOCO", node_defs)
+        out = fragment("CCOCO", node_defs)
 
-        assert graph == truth
+        assert truth in out
 
-    def test_larger_mols(self):
-        node_defs = set()
-        node_defs.add(Group('hydroxyl', 'O', [0]))
-        node_defs.add(Group('keto', 'O', [0,0]))
-        node_defs.add(Group('ester', 'C(=O)O', [0,2]))
-        node_defs.add(Group('methyl', 'C', [0,0,0]))
-        node_defs.add(Group('t2', 'N', [0,0,0]))
-        node_defs.add(Group('secondary_amine', 'CNC', [0,0, 1]))
-
-        truth = GroupGraph()
-        truth.add_node("secondary_amine", "CNC", [0, 0])
-        truth.add_node("t2", "N", [0, 0, 0])
-        truth.add_node("ester", "C(=O)O", [0, 2])
-        truth.add_edge((0, 0), (1, 0))
-        truth.add_edge((1, 0), (2, 0))
-
-        graph = fragment("CNCNOC=O", node_defs)
