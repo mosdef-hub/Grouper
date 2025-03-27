f72b2468b37af76179a6cf565dcadc7823439326
CalCraven <nicholas.c.craven@vanderbilt.edu>
2025-03-26 22:01:31 -0500

    Change nx graph to take nonunique ports, since the hubs have portid based on their index in the hubs list

--- Grouper/utils.py
+++ Grouper/utils.py
@@ -1,62 +1,67 @@
-
-import typing as t
-from typing import Any, Dict, List, Tuple, Union, Callable, Sequence
 from copy import deepcopy
-from Grouper import GroupGraph
+from typing import Any, Callable, Dict, List, Sequence, Tuple
+
 import networkx as nx
 import torch_geometric
 
+from Grouper import GroupGraph
+
+
 def run_performance_eval(
-        nauty_path: str = "/Users/kieran/projects/molGrouper/packages/nauty2_8_8", 
-        node_defs = None,
-        n_runs: int = 3, 
-        max_nodes: int = 6,
-        n_cpus: int = 30,
-        verbose: bool = False
+    nauty_path: str = "/Users/kieran/projects/molGrouper/packages/nauty2_8_8",
+    node_defs=None,
+    n_runs: int = 3,
+    max_nodes: int = 6,
+    n_cpus: int = 30,
+    verbose: bool = False,
 ):
     """
     Run performance evaluation of the Grouper generation featrure with growing sized graphs.
     """
-    import os
-    import time
-    from Grouper import exhaustive_generate
-    import random
     import itertools
+    import random
+    import time
+
+    from Grouper import exhaustive_generate
 
     if node_defs is None:
         raise ValueError("node_defs must be provided.")
-    
+
     # Time the performance of the generation algorithm
-    performance = {combo : { n:[] for n in range(1, max_nodes + 1)} for combo in itertools.combinations(node_defs, len(node_defs) - 1)}
+    performance = {
+        combo: {n: [] for n in range(1, max_nodes + 1)}
+        for combo in itertools.combinations(node_defs, len(node_defs) - 1)
+    }
     for def_combo in performance:
         for n_nodes in range(1, max_nodes + 1):
             for i in range(n_runs):
                 random.seed(0)
                 start_time = time.time()
                 space = exhaustive_generate(
-                    n_nodes, 
-                    set(def_combo), 
+                    n_nodes,
+                    set(def_combo),
                     nauty_path=nauty_path,
                     input_file_path="",
                     num_procs=n_cpus,
-                    verbose=verbose)
+                    verbose=verbose,
+                )
                 end_time = time.time()
                 performance[def_combo][n_nodes].append(end_time - start_time)
             print(f"Number of generated graphs: {len(space)}")
             print("")
     # plot_performance(performance, max_nodes)
     return performance
-    
+
+
 def plot_performance(performance, max_nodes):
     import matplotlib.pyplot as plt
     import numpy as np
-    import itertools
 
     # Prepare data for the plot
     fig, ax = plt.subplots()
-    
+
     # Use a single colormap
-    cmap = plt.get_cmap('inferno')
+    cmap = plt.get_cmap("inferno")
     norm = plt.Normalize()
 
     # Iterate through the performance data to plot
@@ -66,15 +71,11 @@ def plot_performance(performance, max_nodes):
             mean_time = np.mean(times)
             color = cmap(mean_time)
 
-            ax.scatter(
-                n_nodes, 
-                i, 
-                c=color
-            )
+            ax.scatter(n_nodes, i, c=color)
 
     ax.set_xlabel("Number of nodes")
     ax.set_ylabel("Node type combination")
-    
+
     # Adjust y-axis labels to display the node combinations
     ax.set_yticks(range(len(performance)))
     ax.set_yticklabels([str(combo) for combo in performance])
@@ -82,27 +83,30 @@ def plot_performance(performance, max_nodes):
     # Add the color bar
     sm = plt.cm.ScalarMappable(cmap=cmap)
     sm.set_array([])  # Only needed for the color bar
-    plt.colorbar(sm, ax=ax, label='Mean Time (s)')
+    plt.colorbar(sm, ax=ax, label="Mean Time (s)")
 
     plt.savefig("performance.png")
-
 
 
 def convert_edges_to_nodetype(G):
     new_G = deepcopy(G)
     for edge in new_G.edges():
-        for i in range(len(new_G.edges[edge]['ports'])):
-            node_port_str_tuple = new_G.edges[edge]['ports'][i]
+        for i in range(len(new_G.edges[edge]["ports"])):
+            node_port_str_tuple = new_G.edges[edge]["ports"][i]
             src, dst = node_port_str_tuple
-            srcnode, srcport = src.split('.')
-            srcnode_type = new_G.nodes[srcnode]['type']
+            srcnode, srcport = src.split(".")
+            srcnode_type = new_G.nodes[srcnode]["type"]
 
-            dstnode, dstport = dst.split('.')
-            dstnode_type = new_G.nodes[dstnode]['type']
+            dstnode, dstport = dst.split(".")
+            dstnode_type = new_G.nodes[dstnode]["type"]
 
-            new_G.edges[edge]['ports'][i] = (f'{srcnode_type}.{srcport}', f'{dstnode_type}.{dstport}')
+            new_G.edges[edge]["ports"][i] = (
+                f"{srcnode_type}.{srcport}",
+                f"{dstnode_type}.{dstport}",
+            )
 
     return new_G
+
 
 class nxGroupGraph(nx.Graph):
     """A graph with ports as parts of nodes that can be connected to other ports."""
@@ -123,19 +127,17 @@ class nxGroupGraph(nx.Graph):
             node_types = {}
 
         if not isinstance(node_types, dict):
-            raise TypeError("node_types must be a dictionary of node types and their ports")
+            raise TypeError(
+                "node_types must be a dictionary of node types and their ports"
+            )
         if len(node_types) != 0:
-            holder = next(iter(node_types.keys())) # raises error if empty
+            holder = next(iter(node_types.keys()))  # raises error if empty
             holder_type = type(holder)
             if not all(isinstance(k, holder_type) for k in node_types.keys()):
                 raise ValueError("All keys in node_types must be of the same type")
             if not all(isinstance(v, list) for v in node_types.values()):
                 raise ValueError("All values in node_types must be lists")
 
-        for k, v in node_types.items(): # check if ports are unique
-            if len(v) != len(set(v)):
-                raise ValueError("All ports in node_types must be unique")
-            
         self.node_types = node_types
 
     def __str__(self) -> str:
@@ -155,7 +157,7 @@ class nxGroupGraph(nx.Graph):
         - str: Representation of the GroupGraph.
         """
         return f"GroupGraph({','.join(str(tuple(*e[-1])) for e in self.edges.data('ports'))})"
-    
+
     def __eq__(self, other) -> bool:
         """
         Check if two GroupGraph objects are equal.
@@ -168,7 +170,7 @@ class nxGroupGraph(nx.Graph):
         """
         # return self.nodes(data=True) == other.nodes(data=True) and self.edges(data=True) == other.edges(data=True)
         return nx.utils.misc.graphs_equal(self, other)
-    
+
     def __ne__(self, other) -> bool:
         """
         Check if two GroupGraph objects are not equal.
@@ -180,7 +182,7 @@ class nxGroupGraph(nx.Graph):
         - bool: True if not equal, False otherwise.
         """
         return not self.__eq__(other)
-    
+
     def __bool__(self) -> bool:
         """
         Check if the GroupGraph is non-empty.
@@ -190,7 +192,9 @@ class nxGroupGraph(nx.Graph):
         """
         return len(self.nodes) > 0 and len(self.edges) > 0
 
-    def add_node(self, nodeID: Any, node_type: str, smarts: str, hubs: Sequence[int]) -> None:
+    def add_node(
+        self, nodeID: Any, node_type: str, smarts: str, hubs: Sequence[int]
+    ) -> None:
         """
         Add a node to the GroupGraph.
 
@@ -205,12 +209,14 @@ class nxGroupGraph(nx.Graph):
         if nodeID in self.nodes:
             raise AttributeError(f"Node: {nodeID} is already present in Graph")
         super(nxGroupGraph, self).add_node(nodeID)
-        self.nodes[nodeID]['type'] = node_type
-        self.nodes[nodeID]['ports'] = self.node_types[self.nodes[nodeID]['type']]
-        self.nodes[nodeID]['smarts'] = smarts
-        self.nodes[nodeID]['hubs'] = hubs
-    
-    def add_edge(self, node_port_1: Tuple[Any, Any], node_port_2: Tuple[Any, Any]) -> None:
+        self.nodes[nodeID]["type"] = node_type
+        self.nodes[nodeID]["ports"] = self.node_types[self.nodes[nodeID]["type"]]
+        self.nodes[nodeID]["smarts"] = smarts
+        self.nodes[nodeID]["hubs"] = hubs
+
+    def add_edge(
+        self, node_port_1: Tuple[Any, Any], node_port_2: Tuple[Any, Any]
+    ) -> None:
         """
         Add an edge between two nodes in the GroupGraph.
 
@@ -227,40 +233,52 @@ class nxGroupGraph(nx.Graph):
             raise AttributeError(f"Node: {node_port_2[0]} has no free ports!")
         # check if port is already occupied
         for edge in self.edges(data=True):
-            for node_port in edge[-1]['ports']:
-                src_node, src_port = node_port[0].split('.')
-                dst_node, dst_port = node_port[1].split('.')
+            for node_port in edge[-1]["ports"]:
+                src_node, src_port = node_port[0].split(".")
+                dst_node, dst_port = node_port[1].split(".")
                 if src_node == node_port_1[0]:
                     if src_port == node_port_1[1]:
-                        raise AttributeError(f"Node: {node_port_1[0]}.{node_port_1[1]} is already occupied!")
+                        raise AttributeError(
+                            f"Node: {node_port_1[0]}.{node_port_1[1]} is already occupied!"
+                        )
                 if src_node == node_port_2[0]:
                     if src_port == node_port_2[1]:
-                        raise AttributeError(f"Node: {node_port_2[0]}.{node_port_2[1]} is already occupied!")
+                        raise AttributeError(
+                            f"Node: {node_port_2[0]}.{node_port_2[1]} is already occupied!"
+                        )
                 if dst_node == node_port_1[0]:
                     if dst_port == node_port_1[1]:
-                        raise AttributeError(f"Node: {node_port_1[0]}.{node_port_1[1]} is already occupied!")
+                        raise AttributeError(
+                            f"Node: {node_port_1[0]}.{node_port_1[1]} is already occupied!"
+                        )
                 if dst_node == node_port_2[0]:
                     if dst_port == node_port_2[1]:
-                        raise AttributeError(f"Node: {node_port_2[0]}.{node_port_2[1]} is already occupied!")
-                
+                        raise AttributeError(
+                            f"Node: {node_port_2[0]}.{node_port_2[1]} is already occupied!"
+                        )
 
         edge_ports = []
 
-        for n, p in [(node_port_1[0], node_port_1[1]), (node_port_2[0], node_port_2[1])]:
+        for n, p in [
+            (node_port_1[0], node_port_1[1]),
+            (node_port_2[0], node_port_2[1]),
+        ]:
             # Sanity check to see if the nodes and ports are present in Graph
             if n not in self.nodes:
                 raise AttributeError(f"Node: {p} is not present in Graph")
-            if p not in self.nodes(data=True)[n]['ports']:
+            if p not in self.nodes(data=True)[n]["ports"]:
                 raise AttributeError(f"Port: {p} is incorrect for Node: {n}!")
 
-            edge_ports.append(str(n) + '.' + str(p))
+            edge_ports.append(str(n) + "." + str(p))
 
         # Add the port points as edge attributes
         if self.has_edge(node_port_1[0], node_port_2[0]):
-            self.edges[node_port_1[0], node_port_2[0]]['ports'].append(edge_ports)
+            self.edges[node_port_1[0], node_port_2[0]]["ports"].append(edge_ports)
         else:
-            super(nxGroupGraph, self).add_edge(node_port_1[0], node_port_2[0], ports=[edge_ports])
-        
+            super(nxGroupGraph, self).add_edge(
+                node_port_1[0], node_port_2[0], ports=[edge_ports]
+            )
+
         # remove ports from portsList
 
     def make_undirected(self):
@@ -270,12 +288,12 @@ class nxGroupGraph(nx.Graph):
         Adds bonds and updates edge ports accordingly.
         """
         for edge in self.edges:
-            node_port = tuple(self.edges[edge]['ports'][0])
+            node_port = tuple(self.edges[edge]["ports"][0])
             node_s, node_t = edge
-            port_s, port_t = edge['ports'][0], edge['ports'][1]
+            port_s, port_t = edge["ports"][0], edge["ports"][1]
             self.add_bond(node_t, port_t, node_s, port_s)
-            self.edges[edge]['ports'].append(f'{node_port[1]}.{node_port[0]}')
-        
+            self.edges[edge]["ports"].append(f"{node_port[1]}.{node_port[0]}")
+
     def n_free_ports(self, nodeID: Any) -> int:
         """
         Get the number of free ports on a node.
@@ -289,14 +307,23 @@ class nxGroupGraph(nx.Graph):
         # num ports - num edges - num edges with node as target
         occupied_ports = 0
         if len(self.edges()) == 0:
-            return len(self.nodes[nodeID]['ports'])
+            return len(self.nodes[nodeID]["ports"])
         for e in self.edges(data=True):
-            for node_port in e[-1]['ports']:
-                if node_port[0].split('.')[0] == nodeID or node_port[1].split('.')[0] == nodeID:
+            for node_port in e[-1]["ports"]:
+                if (
+                    node_port[0].split(".")[0] == nodeID
+                    or node_port[1].split(".")[0] == nodeID
+                ):
                     occupied_ports += 1
-        return len(self.nodes[nodeID]['ports']) - occupied_ports # total number of ports - number of ports with edges
-    
-    def to_PyG_Data(self, node_descriptor_generator: Callable[[str], Sequence[float]], max_ports: int = 0) -> torch_geometric.data.Data:
+        return (
+            len(self.nodes[nodeID]["ports"]) - occupied_ports
+        )  # total number of ports - number of ports with edges
+
+    def to_PyG_Data(
+        self,
+        node_descriptor_generator: Callable[[str], Sequence[float]],
+        max_ports: int = 0,
+    ) -> torch_geometric.data.Data:
         """
         Convert the GroupGraph to a PyG Data representation.
 
@@ -306,48 +333,55 @@ class nxGroupGraph(nx.Graph):
         Returns:
         - torch_geometric.data.Data: PyG Data representation.
         """
-        from torch_geometric.data import Data
         import torch
-        one_hot_vector = lambda index, num_classes : torch.eye(num_classes)[index]
-        max_ports = max(len(v) for v in self.node_types.values()) if max_ports == 0 else max_ports
+        from torch_geometric.data import Data
 
+        one_hot_vector = lambda index, num_classes: torch.eye(num_classes)[index]
+        max_ports = (
+            max(len(v) for v in self.node_types.values())
+            if max_ports == 0
+            else max_ports
+        )
 
         # Create the node features
-        dummy_feature = node_descriptor_generator(self.nodes(data=True)[0]['smarts'])
-        node_features = torch.zeros((len(self.nodes), len(dummy_feature))).to(torch.float64)
+        dummy_feature = node_descriptor_generator(self.nodes(data=True)[0]["smarts"])
+        node_features = torch.zeros((len(self.nodes), len(dummy_feature))).to(
+            torch.float64
+        )
         for i, n in enumerate(self.nodes(data=True)):
-            node_features[i] = node_descriptor_generator(n[1]['smarts'])
-        
+            node_features[i] = node_descriptor_generator(n[1]["smarts"])
+
         # Create the edge index
         edge_index = torch.zeros((2, len(self.edges)), dtype=torch.long)
         for i, e in enumerate(self.edges):
             edge_index[0, i] = list(self.nodes).index(e[0])
             edge_index[1, i] = list(self.nodes).index(e[1])
-        
+
         # Create the edge features
-        edge_features = torch.zeros(len(self.edges), max_ports*2)
+        edge_features = torch.zeros(len(self.edges), max_ports * 2)
 
         for i, e in enumerate(self.edges(data=True)):
-            
             # get nodes and ports
-            node_ports = [node_port.split('.') for node_port in e[2]['ports'][0]]
+            node_ports = [node_port.split(".") for node_port in e[2]["ports"][0]]
 
             # Convert node_ports to int from string
             node_ports = [(int(node), int(port)) for node, port in node_ports]
 
-            
             # convert the ports to one-hot vectors
-            port_s = self.nodes(data=True)[node_ports[0][0]]['ports']
-            port_t = self.nodes(data=True)[node_ports[1][0]]['ports']
+            port_s = self.nodes(data=True)[node_ports[0][0]]["ports"]
+            port_t = self.nodes(data=True)[node_ports[1][0]]["ports"]
             port_index_s = port_s.index(node_ports[0][1])
             port_index_t = port_t.index(node_ports[1][1])
-            edge_features[i] = torch.cat([
-                one_hot_vector(port_index_s, max_ports), 
-                one_hot_vector(port_index_t, max_ports)
-            ]).to(torch.float64)
+            edge_features[i] = torch.cat(
+                [
+                    one_hot_vector(port_index_s, max_ports),
+                    one_hot_vector(port_index_t, max_ports),
+                ]
+            ).to(torch.float64)
 
         return Data(x=node_features, edge_index=edge_index, edge_attr=edge_features)
-    
+
+
 def convert_to_nx(G: GroupGraph) -> nxGroupGraph:
     """
     Convert a GroupGraph to a networkx graph.
@@ -364,12 +398,7 @@ def convert_to_nx(G: GroupGraph) -> nxGroupGraph:
         return nxGroupGraph()
     nxG = nxGroupGraph(G.node_types)
     for node_id, node in G.nodes.items():
-        nxG.add_node(
-            node_id,
-            node.type,
-            node.smarts,
-            node.hubs
-        )
+        nxG.add_node(node_id, node.type, node.smarts, node.hubs)
     for edge in G.edges:
         src = (edge[0], edge[1])
         dst = (edge[2], edge[3])
