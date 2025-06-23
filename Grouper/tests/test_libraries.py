3f182f6a702aa2bfa01f728f362c6289bad55b8f
CalCraven <nicholas.c.craven@vanderbilt.edu>
2025-06-23 08:10:45 -0500

    Finish UNIFAC groups testing

--- Grouper/tests/test_libraries.py
+++ Grouper/tests/test_libraries.py
@@ -23,7 +23,7 @@ Libraries = {
 class TestLibraries(BaseTest):
     @pytest.mark.parametrize(
         "library,n_graphs",
-        [("saftgm", 1), ("joback", 41), ("UNIFAC", 70), ("base", 0)],
+        [("saftgm", 1), ("joback", 41), ("UNIFAC", 69), ("base", 0)],
     )
     def test_build_library(self, library, n_graphs):
         library = Libraries[library]()
@@ -77,29 +77,39 @@ class TestLibrariesFragmentations(BaseTest):
 
     # TODO: How to handle "CN", which has a UNIFAC Group GCPair(raw"[CX4;H3][NX3;H2]","CH3NH2"), but no ports
     # TODO: More failing molecules that I tried to test -NCC
-    unifac_fail_molecules = ["C(=O)N", "OCC(=O)N(C)C", "C(=O)N(C)C", "CN", "C[O-]", "CCC(=O)[O-]", "CCS(=O)(=O)[O-]", "CCC#N", "C(=O)[N-]"] # bases
+    unifac_fail_molecules = [
+        "C(=O)N", "OCC(=O)N(C)C", "C(=O)N(C)C", "CN", 
+        "C[O-]", "CCC(=O)[O-]", "CCS(=O)(=O)[O-]", 
+        "CCC#N", "C(=O)[N-]", "CO"
+    ]
     test_molecules = [ # NOTE that "C(=O)N", or formaldehyde doesn't work
         "CC", "CC(C)C", "CCC(C)(C)C", "CC(C)(C)C",# alkanes
-        "CO", "OCC(O)CO", # alcohols
+        "OCC(O)CO", # alcohols
         "O=CO", "COC(C)=O", "OCCCC(=O)O", "CC(N)=O", "CNC(C)=O", # carboxyls/amides
         "CNC", "CNCNC",# amines
         "FC(F)F", "NCCCCF",# fluorines
-        "C1CCCCC1C","C1CCCC(C)C1C", "C1CCC(C)CC1C", "C1CC(C)CCC1C",# rings
-        "CC1=C(C=C(C=C1[N+](=O)[O-])[N+](=O)[O-])[N+](=O)[O-]", "C1=CC=C(C=C1)C(=O)O",# phenyls
+        "CC1CCCCC1", "CC1CCCCC1C", "CC1CCCC(C)C1", "CC1CCC(C)CC1",# rings
+        "Cc1c([N+](=O)[O-])cc([N+](=O)[O-])cc1[N+](=O)[O-]", "c1ccc(cc1)C(=O)O",# phenyls
     ]
     groupsList = [
-        {"CH3":2}, {"CH":1, "CH3":3}, {"CH3":4, "CH2":1, "C":1}, {"CH3":4, "C":1},
-        {"CH3OH":1}, {"OH (P)":2, "CH2":2, "CH":1, "OH (S)":1},
-        {"HCOO":1}, {"CH3COO":1, "CH3":1}, {"CH2COO":1, "CH2":2, "OH (P)":1}, {"CONH2":1, "CH3":1}, {"CONHCH3":1, "CH3":1},
+        {"CH3":2}, {"CH":1, "CH3":3}, {"CH3":4, "CH2":1, "C":1}, {"CH3":4, "C":1}, # alkanes
+        {"OH (P)":2, "CH2":2, "CH":1, "OH (S)":1}, # alcohols
+        {"HCOO":1}, {"CH3COO":1, "CH3":1}, {"CH2COO":1, "CH2":2, "OH (P)":1}, {"CONH2":1, "CH3":1}, {"CONHCH3":1, "CH3":1}, #carboxyl/amides
         {"CH3":1, "CH3NH":1}, {"CH2":1, "CH3NH":2},  # amines
         {"CF3":1}, {"CH2NH2":1, "CH2":2, "CF":1}, # fluorines
+        {"CY-CH2":5, "CH3":1, "CY-CH":1}, {"CY-CH2":4, "CH3":2, "CY-CH":2}, # rings
+        {"CY-CH2":4, "CH3":2, "CY-CH":2}, {"CY-CH2":4, "CH3":2, "CY-CH":2},# rings
+        {"ACCH3":1, "ACH":2, "ACNO2":3}, {"ACH":5, "AC":1, "COOH":1}, # phenyl groups
         ]
     edgesList = [
-        {("CH3","CH3"):1}, {("CH3","CH"):3}, {("CH3","CH2"):1, ("CH3","C"):3, ("CH2","C"):1}, {("C","CH3"):4},
-        {}, {("OH (P)", "CH2"):2, ("OH (S)", "CH"):1, ("CH2", "CH"):2},
-        {}, {("CH3COO", "CH3"):1}, {("CH2COO", "CH2"):1, ("CH2", "CH2"):1, ("CH2", "OH (P)"):1}, {("CONH2",  "CH3"):1}, {("CONHCH3", "CH3"):1},
+        {("CH3","CH3"):1}, {("CH3","CH"):3}, {("CH3","CH2"):1, ("CH3","C"):3, ("CH2","C"):1}, {("C","CH3"):4}, # alkanes
+        {("OH (P)", "CH2"):2, ("OH (S)", "CH"):1, ("CH2", "CH"):2}, # alcohols
+        {}, {("CH3COO", "CH3"):1}, {("CH2COO", "CH2"):1, ("CH2", "CH2"):1, ("CH2", "OH (P)"):1}, {("CONH2",  "CH3"):1}, {("CONHCH3", "CH3"):1}, #carbxoyls/amides
         {("CH3", "CH3NH"):1}, {("CH2", "CH3NH"):2},  #amines
-        {}, {("CH2NH2","CH2"):1,("CH2", "CF"):1, ("CH2", "CH2"):1}# fluorines
+        {}, {("CH2NH2","CH2"):1,("CH2", "CF"):1, ("CH2", "CH2"):1}, # fluorines
+        {("CY-CH2", "CY-CH2"):4, ("CH3", "CY-CH"):1, ("CY-CH2", "CY-CH"):2}, {("CY-CH2", "CY-CH2"):3, ("CH3", "CY-CH"):2, ("CY-CH2", "CY-CH"):2, ("CY-CH", "CY-CH"):1}, # rings
+        {("CY-CH2", "CY-CH2"):2, ("CH3", "CY-CH"):2, ("CY-CH2", "CY-CH"):4},  {("CY-CH2", "CY-CH2"):2, ("CH3", "CY-CH"):2, ("CY-CH2", "CY-CH"):4},# rings
+        {("ACCH3", "ACNO2"):2, ("ACNO2", "ACH"):4}, {("ACH", "ACH"):4, ("AC", "ACH"):2, ("AC", "COOH"):1}# phenyl groups
     ]
 
     @pytest.fixture(scope="session")
@@ -114,8 +124,9 @@ class TestLibrariesFragmentations(BaseTest):
         # molecule = test_molecules[0]
         from Grouper import fragment
         gGList = fragment(MOLSMILES, unifac_groups, returnHandler="ideal", incompleteGraphHandler="remove", nodeDefsSorter="size")
+        # may have to consider this just to be 1
+        assert len(gGList) >= 1, print(fragment(MOLSMILES, unifac_groups, returnHandler="ideal", incompleteGraphHandler="keep", nodeDefsSorter="size"))
         firstgG = gGList[0]
-        assert len(gGList) >= 1 # may have to consider this just to be 1
         assert is_connected(convert_to_nx(firstgG).to_undirected())
         assert firstgG.to_smiles() == MOLSMILES
         groupsSmarts = Counter([group.type for group in firstgG.nodes.values()])
