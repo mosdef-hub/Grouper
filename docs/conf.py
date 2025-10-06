d1b32451e09e3b918a37acfcef15431967f50ef3
kierannp <nehilkieran@gmail.com>
2025-10-06 14:18:09 -0500

    Update documentation workflow

---
+++ docs/conf.py
@@ -0,0 +1,25 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
+]
+
+autodoc_mock_imports = ['numpy', 'pandas', 'pyarrow']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
